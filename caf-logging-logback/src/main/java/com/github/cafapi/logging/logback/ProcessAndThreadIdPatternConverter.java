de4aa2dcb6ec0feef86c1ea77a113b6578c2addd
thilagasanthosh <thilagavathia@gmail.com>
2020-01-06 09:41:36 +0000

    SCMOD-7821: caf-logging-logback-access moved to opensuse-tomcat-image (#11)
    * SCMOD-7821: caf-logging-logback-access moved to opensuse-tomcat-image
    
    * Revert "SCMOD-7821: caf-logging-logback-access moved to opensuse-tomcat-image"
    
    This reverts commit 6c96326155dd99896aedf9506bd1ce69378a3acc.
    
    * SCMOD-7821: Logback classic changes for tomcat logging
    
    * Whitespace: Use same indenting as the rest of the project
    
    * Make parameters final
    
    * Renamed converter to be consistent with the log4j module
    
    * Renamed conversion word to be consistent with the log4j module
    
    * Removed unnecessary LoggingEvent dependency
    
    Co-authored-by: Dermot Hardy <dermot.hardy@microfocus.com>

--- caf-logging-logback/src/main/java/com/github/cafapi/logging/logback/ProcessAndThreadIdPatternConverter.java
+++ caf-logging-logback/src/main/java/com/github/cafapi/logging/logback/ProcessAndThreadIdPatternConverter.java
@@ -13,23 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.cafapi.logging.logback.access;
+package com.github.cafapi.logging.logback;
 
-import ch.qos.logback.access.spi.AccessEvent;
-import ch.qos.logback.core.ConsoleAppender;
-
+import ch.qos.logback.core.pattern.DynamicConverter;
 import com.github.cafapi.logging.common.ProcessAndThreadIdProvider;
 
-public final class CAFAccessLogConsoleAppender extends ConsoleAppender<AccessEvent>
+public final class ProcessAndThreadIdPatternConverter<E> extends DynamicConverter<E>
 {
-    public CAFAccessLogConsoleAppender()
-    {
-    }
-
     @Override
-    protected void subAppend(final AccessEvent event)
+    public String convert(final E event)
     {
-        event.setThreadName(ProcessAndThreadIdProvider.getId());
-        super.subAppend(event);
+        return ProcessAndThreadIdProvider.getId();
     }
 }
