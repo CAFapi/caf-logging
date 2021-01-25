04efd17755a30f44c71faeb90a4f4d1c09307ec1
Simranjeet Singh Chawla <Simranjeet.SinghChawla@microfocus.com>
2021-01-25 17:27:21 +0530

    Creating a library to include dropwizard bundle & sprint boot filter for logging correlation id

---
+++ caf-correlation-id/src/main/java/com/microfocus/http/filters/correlationid/CorrelationIdBundle.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019-2020 Micro Focus or one of its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.microfocus.http.filters.correlationid;
+
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+
+import io.dropwizard.Configuration;
+import io.dropwizard.ConfiguredBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class CorrelationIdBundle<C extends Configuration> implements ConfiguredBundle<C> {
+    
+    @Override
+    public void initialize(Bootstrap<?> bootstrap) {
+    
+    }
+    
+    @Override
+    public void run(C configuration, Environment environment) {
+        environment.servlets()
+                .addFilter("correlation-id-servlet-filter", new CorrelationIdFilter())
+                .addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), false, "*");
+    }
+}
---
+++ caf-correlation-id/src/main/java/com/microfocus/http/filters/correlationid/CorrelationIdFilter.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2019-2020 Micro Focus or one of its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.microfocus.http.filters.correlationid;
+
+import static com.microfocus.http.filters.correlationid.CorrelationIdConfigurationConstants.headerName;
+import static com.microfocus.http.filters.correlationid.CorrelationIdConfigurationConstants.mdcKey;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.UUID;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CorrelationIdFilter implements Filter {
+    
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        String correlationId = Optional.ofNullable(req.getHeader(headerName)).orElseGet(() -> UUID.randomUUID().toString());
+        MDC.put(mdcKey, correlationId);
+        resp.addHeader(headerName, correlationId);
+        chain.doFilter(request, response);
+    }
+    
+    @Override
+    public void destroy() {
+    }
+    
+}
